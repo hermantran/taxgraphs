015bf99f1304322300b95a25cfdf6804fb797a52
hermantran <hermanctran@gmail.com>
2014-10-05 14:59:37 -0700

    added tax data for all remaining states

--- tasks/config/json-bake.js
+++ tasks/config/json-bake.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.config.set('json_bake', {
     dist: {
       files: {
-        'data/2014.json': 'data/base.json'
+        'data/2014.json': 'data/template.json'
       }
     }
   });
