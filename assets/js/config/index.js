3c8d302ac7167306b86f03f1d7200960b9f9f151
Herman Tran <hermanctran@gmail.com>
2017-10-18 00:36:46 -0700

    added cache busting for tax data json

--- assets/js/config/index.js
+++ assets/js/config/index.js
@@ -3,20 +3,20 @@
 var app = require('../app'),
     d3 = require('d3'),
     lodash = require('lodash'),
-    JST = require('JST'),
     saveService = require('saveSvgAsPng/saveSvgAsPng'),
     ga = require('ga'),
     routes = require('./routes'),
     rootScope = require('./rootScope'),
-    domain = require('../../../package.json').homepage;
+    project = require('../../../package.json'),
+    domain = project.homepage,
+    version = project.version;
 
 app.constant('d3', d3)
   .constant('_', lodash)
-  .constant('JST', JST)
   .constant('saveService', saveService)
   .constant('ga', ga)
   .constant('GA_TRACKING_ID', 'UA-55615931-1')
-  .constant('TAX_API', 'data/taxes.json')
+  .constant('TAX_API', 'data/taxes.json?v=' + version)
   .constant('DOMAIN', domain)
   .config(routes)
   .run(rootScope);
